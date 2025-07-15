75a523891b203985c61e9fc3ecf6489a22928e95
Menezess42 <arielmenezess@hotmail.com>
2025-07-15 14:34:57 -0300

    Removing obstuido

--- configuration.nix
+++ configuration.nix
@@ -62,7 +62,7 @@
     };
     environment.sessionVariables = {WL_NO_HARDWARE_CURSOR="1"; NIXOS_OZONE_WL="1";};
     xdg.portal.enable = true;
-    xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-gtk  pkgs.xdg-desktop-portal-wlr ];
+    # xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-gtk  pkgs.xdg-desktop-portal-wlr ];
 
 
 # Configure keymap in X11
