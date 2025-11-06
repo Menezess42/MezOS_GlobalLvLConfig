71a801b0bcc419282cd15f2f71bc32ae9f6ccfd1
Menezess42 <arielmenezess@hotmail.com>
2025-11-06 12:09:19 -0300

    core limitd

--- configuration.nix
+++ configuration.nix
@@ -184,16 +184,10 @@ environment.sessionVariables.NIXOS_OZONE_WL="1";
 
 nix.settings = {
 # Limita o número de *build jobs* simultâneos (processos paralelos de build)
-    max-jobs = 1;
+    max-jobs = 3;
 
 # Limita os núcleos usados por cada build individual
-    cores = 4;
-
-# Opcional: reduz prioridade de builds para não travar o sistema
-    nice = 10;
-
-# Opcional: ativa compressão leve de logs e resultados (menos carga de I/O)
-    log-compression = true;
+    cores = 3;
 };
 
 
